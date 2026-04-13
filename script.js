a2eb4278ed71bf8e136112edf0fd97e54d24e6f4
CaydenArlett <caydenarlett15@gmail.com>
2026-04-13 20:31:25 +0000

    Add difficulty radio buttons to html and comment plan also made the java script for the start button which generates a random word based on the selected difficulty

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// add javascript here
+var randomWord = "";
+function getE(selectedId){
+    var selectedElement = document.getElementById(selectedId);
+    return selectedElement;
+}
+//start game
+function startGame() {
+    //enables/dissables buttons
+    getE("startBtn").disabled = true;
+    getE("guessBtn").disabled = false;
+    //generates a random word based on the difficulty selected
+    var easyWordBank = ["apple", "beach", "bread", "brush", "chair", "chest", "cloud", "dance", "dream", "drink", "earth", "field", "floor", "fruit", "glass", "grape", "green", "heart", "horse", "house", "juice", "light", "lunch", "money", "month", "music", "night", "ocean", "party", "phone", "piano", "pilot", "plant", "plate", "point", "power", "radio", "river", "scene", "shirt", "sleep", "smile", "sound", "space", "spoon", "table", "train", "truck", "water", "world"];
+    var mediumWordBank = ["abode", "ample", "birch", "blimp", "bosch", "brave", "brick", "brisk", "chasm", "chime", "chunk", "cliff", "clout", "crane", "crisp", "dwelt", "elbow", "fancy", "fjord", "flask", "flick", "frank", "frost", "gland", "glyph", "gnarl", "gourd", "graft", "grasp", "hound", "joint", "knelt", "knock", "lynch", "morph", "night", "oxide", "phial", "prowl", "quake", "scalp", "snarl", "spelt", "spout", "squat", "thump", "twirl", "vague", "wharf", "wrest"];
+    var hardWordBank = ["abyss", "askew", "azure", "bagel", "banjo", "bayou", "blitz", "buxom", "crypt", "cycle", "dizzy", "duchy", "dwarf", "equip", "fazed", "fjord", "fluff", "frizz", "funny", "gawky", "glyph", "gumbo", "haiku", "hertz", "hyena", "ivory", "jazzy", "jelly", "jiffy", "joker", "jumbo", "kayak", "kazoo", "kinky", "klutz", "knack", "lymph", "mummy", "nymph", "ovary", "pixel", "pizazz", "pneum", "polka", "quartz", "queue", "quips", "rhino", "rhythm", "vixen"];
+    var wordBank = [easyWordBank, mediumWordBank, hardWordBank];
+    var difficulty = getE("difficulty").value;
+    if (difficulty == "easy") {
+        randomWord = wordBank[difficulty][Math.floor(Math.random() * wordBank[difficulty].length)];
+    } else if (difficulty == "medium") {
+        randomWord = wordBank[difficulty][Math.floor(Math.random() * wordBank[difficulty].length)];
+    } else if (difficulty == "hard") {
+        randomWord = wordBank[difficulty][Math.floor(Math.random() * wordBank[difficulty].length)];
+    }
+    console.log(randomWord);
+}
+
+getE("startBtn").addEventListener("click", startGame);
